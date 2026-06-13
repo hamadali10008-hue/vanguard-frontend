8fcd500f663f36c43b6bc1d69900a2c05835d556
Hamad Ali <hamadali10008@gmail.com>
2026-06-13 18:11:22 +0100

    update build vercel

--- saas-frontend/src/app/login/page.tsx
+++ saas-frontend/src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
 
     try {
 
-     const response = await axios.post('https://Saas-Load-Balancer-684774742.us-east-1.elb.amazonaws.com/api/Users/login', { email, password });
+     const response = await axios.post('/api/Users/login', { email, password });
       
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('tenantId', response.data.tenantId);
